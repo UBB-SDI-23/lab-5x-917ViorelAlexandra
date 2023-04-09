577c3b9315728d20a472b5b4216a7554eb845862
917ViorelAlexandra <alexandra.viorel@stud.ubbcluj.ro>
2023-04-09 19:52:14 +0300

    modified show all

--- react-frontend/src/components/tennisPlayer/TennisPlayerShowAll.tsx
+++ react-frontend/src/components/tennisPlayer/TennisPlayerShowAll.tsx
@@ -93,7 +93,7 @@ export const TennisPlayerShowAll = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {tennisPlayers.slice(0, 100).map((tennisPlayer:TennisPlayer, index) => (
+                        {tennisPlayers.map((tennisPlayer:TennisPlayer, index) => (
                             <TableRow key={tennisPlayer.id}>
                                 <TableCell component="th" scope="row">
                                     {index + 1}
