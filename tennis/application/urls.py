c8af6c45069662fc20c972c4a56dcc24e8613eca
917ViorelAlexandra <alexandra.viorel@stud.ubbcluj.ro>
2023-04-20 14:08:21 +0300

    .

--- tennis/application/urls.py
+++ tennis/application/urls.py
@@ -1,22 +1,22 @@
 from django.urls import path
 
-from .Views.CoachViews import CoachDetail, CoachInfo, CoachesWithAtLeastNYearsExperience
+from .Views.CoachViews import CoachListCreateView, CoachInfo, CoachesWithAtLeastNYearsExperience
 from .Views.TennisPlayerViews import TennisPlayerListCreateView, TennisPlayerInfo, TennisPlayerCoachListInfo, \
     PlayersByAvgYearsOfExperienceOfCoaches, PlayersRegisteredInGrandSlams
-from .Views.TournamentRegistrationViews import TournamentRegistrationDetail, TournamentRegistrationInfo
-from .Views.TournamentViews import TournamentInfo, TournamentDetail
+from .Views.TournamentRegistrationViews import TournamentRegistrationListCreateView, TournamentRegistrationInfo
+from .Views.TournamentViews import TournamentInfo, TournamentListCreateView
 
 urlpatterns = [
 
     path("tennisplayer/", TennisPlayerListCreateView.as_view(), name="tennisplayer"),
     path("tennisplayer/<int:id>/", TennisPlayerInfo.as_view()),
     path("tennisplayer/<int:id>/coach/", TennisPlayerCoachListInfo.as_view()),
-    path("coach/", CoachDetail.as_view(), name="coach"),
+    path("coach/", CoachListCreateView.as_view(), name="coach"),
     path("coach/<int:id>/", CoachInfo.as_view()),
     path("yearsofexp/<int:yoe>/", CoachesWithAtLeastNYearsExperience.as_view(), name="yoe"),
-    path("tournament/", TournamentDetail.as_view(), name="tournament"),
+    path("tournament/", TournamentListCreateView.as_view(), name="tournament"),
     path("tournament/<int:id>/", TournamentInfo.as_view()),
-    path("tournamentreg/", TournamentRegistrationDetail.as_view(), name="tournamentreg"),
+    path("tournamentreg/", TournamentRegistrationListCreateView.as_view(), name="tournamentreg"),
     path("tournamentreg/<int:id>/", TournamentRegistrationInfo.as_view()),
     path("playeravg/", PlayersByAvgYearsOfExperienceOfCoaches.as_view()),
     path("playergs/", PlayersRegisteredInGrandSlams.as_view()),
