7175f41f4a8f59bac169a1921c8577fcf5d28da8
917ViorelAlexandra <alexandra.viorel@stud.ubbcluj.ro>
2023-04-22 19:07:51 +0300

    aggregate value

--- react-frontend/src/components/tennisPlayer/TennisPlayerShowAll.tsx
+++ react-frontend/src/components/tennisPlayer/TennisPlayerShowAll.tsx
@@ -113,6 +113,7 @@ export const TennisPlayerShowAll = () => {
                                 <TableCell align="center">{tennisPlayer.tp_country}</TableCell>
                                 <TableCell align="center">{tennisPlayer.tp_gender}</TableCell>
                                 <TableCell align="center">{tennisPlayer.nb_coaches}</TableCell>
+                                <TableCell align="center">{tennisPlayer.nb_registers}</TableCell>
                                 <TableCell align="right">
 										<IconButton
 											component={Link}
